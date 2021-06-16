60afffa298a958736c294655d6c7863f19475fc1
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-06-17 06:08:59 +0900

    投稿の一覧を仮表示

---
+++ posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import PostsListView
+
+
+app_name = "posts"
+
+urlpatterns = [
+    path('', PostsListView.as_view(), name="posts-list"),
+]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+
+class PostsListView(ListView):
+    model = Post
+    queryset = Post.objects.all()
+    context_object_name = "posts"
